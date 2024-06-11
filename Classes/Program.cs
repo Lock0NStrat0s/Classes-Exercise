919078f5ff8091569584c00a556c0c6a67e2fddc
Mohammad Jabir <mohammadj94@gmail.com>
2024-06-10 19:48:43 -0600

    exercise complete

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car Class = new Car();
+            Class.Make = "GMC";
+            Class.Model = "Terrain";
+            Class.Year = 2014;
+
+            Console.WriteLine($"Make: {Class.Make}");
+            Console.WriteLine($"Model: {Class.Model}");
+            Console.WriteLine($"Year: {Class.Year}");
         }
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
     }
 }
